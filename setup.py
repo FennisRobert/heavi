61ce259de492339087051e7ee37820d437d62aa3
EMerge <emerge@MacBookPro.home>
2025-02-05 09:37:04 +0100

    "Bump version: 0.4.0 → 0.4.1"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='heavi',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(where='src', include=["heavi", "heavi.*"]),
     package_dir={'': 'src'},
     install_requires=[
