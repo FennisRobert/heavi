a214090806583dcd4cbfc0f3dc567073b9d8fb2c
EMerge <emerge@MacBookPro.home>
2025-03-08 17:29:54 +0100

    upload to pypi version

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='heavi',
     version='0.5.0',
+    description='A simple python based linear circuit simulator.',
+    author="Robert Fennis",
     packages=find_packages(where='src', include=["heavi", "heavi.*"]),
     package_dir={'': 'src'},
     install_requires=[
@@ -11,6 +13,12 @@ setup(
         "matplotlib",
         "loguru",
         "sympy",
+    ],
+    lisence="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=3.10",
 )
