a71629f3fdbcb97ec32945863b2a6bc6b1c766cc
EMerge <emerge@MacBookPro.home>
2025-04-01 11:35:43 +0200

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='heavi',
-    version='0.7.2',
+    version='0.7.3',
     description='A simple python based linear circuit simulator.',
     author="Robert Fennis",
     packages=find_packages(where='src', include=["heavi", "heavi.*"]),
